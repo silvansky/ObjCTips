c7dd263e134c9e021e26e95bbff3a3b7c025faf3
silvansky <v.silvansky@gmail.com>
2014-01-28 11:02:32 +0400

    Initial Commit

---
+++ ObjCTips/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ObjCTips
+//
+//  Created by Valentine on 28.01.14.
+//  Copyright (c) 2014 silvansky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
